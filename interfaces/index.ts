6f3a74641aa12b3e6227f135e2d3fad3d71a896c
Hanna Tariku <hannatariku1996@gmail.com>
2025-07-20 20:28:33 +0300

    initial commit

---
+++ interfaces/index.ts
@@ -0,0 +1,25 @@
+export interface CardProps{}
+export interface ButtonProps{}
+
+export interface PropertyAddress {
+  state: string;
+  city: string;
+  country: string;
+}
+
+export interface PropertyOffers {
+  bed: string;
+  shower: string;
+  occupants: string;
+}
+
+export interface PropertyProps {
+  name: string;
+  address: PropertyAddress;
+  rating: number;
+  category: string[];
+  price: number;
+  offers: PropertyOffers;
+  image: string;
+  discount: string;
+}
